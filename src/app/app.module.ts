c05cff96d1c80b921ab25f2edc9151eec3436603
mayurrohokale <mayurrohokale12345@gmail.com>
2024-01-30 19:09:57 +0530

    reorganising header and catnavigation into components hierarchey

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './home/components/header/header.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontawesomeObject } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { CatnavigationComponent } from './catnavigation/catnavigation.component';
+import { CatnavigationComponent } from './home/components/catnavigation/catnavigation.component';
 import { HomeModule } from './home/home.module';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    CatnavigationComponent
+    
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { catchError } from 'rxjs';
+import { CatnavigationComponent } from './components/catnavigation/catnavigation.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    CatnavigationComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FontAwesomeModule
   ]
 })
 export class HomeModule { }
