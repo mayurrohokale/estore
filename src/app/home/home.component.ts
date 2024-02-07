3d3a84f918aff37c56622f9b84dac9444d6b1e24
mayurrohokale <mayurrohokale12345@gmail.com>
2024-02-07 12:41:58 +0530

    navigate to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,15 +2,26 @@ import { Component } from '@angular/core';
 import { CategoriesStoreItem } from './services/category/categories.storeItem';
 import { ProductsStoreItem } from './services/product/products.storeItem';
 import { SearchKeyword } from './types/searchKeyword.type';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  constructor(private categoriesStoreItem: CategoriesStoreItem, private productsStoreItem: ProductsStoreItem){
+  constructor(private categoriesStoreItem: CategoriesStoreItem, private productsStoreItem: ProductsStoreItem,
+    private router: Router){
     this.categoriesStoreItem.loadCategories();
     this.productsStoreItem.loadProducts();
+    router.events
+    .pipe(filter((event) => event instanceof NavigationEnd))
+    .subscribe(event => {
+      if((event as NavigationEnd).url === '/home'){
+        router.navigate(['/home/products']);
+      }
+    });
   }
 
  
