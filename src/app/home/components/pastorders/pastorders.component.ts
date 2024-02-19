4f5b9bb756136486bcdd3b046ab2a95c46682aa1
mayurrohokale <mayurrohokale12345@gmail.com>
2024-02-19 14:31:20 +0530

    past order displayed in login tab

--- src/app/home/components/pastorders/pastorders.component.ts
+++ src/app/home/components/pastorders/pastorders.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PastOrder, PastOrderProduct } from '../../types/order.type';
 
 @Component({
   selector: 'app-pastorders',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./pastorders.component.scss']
 })
 export class PastordersComponent {
+  pastOrderProducts: PastOrderProduct[] = [
+    {
+      amount: 100,
+      price: 50,
+      productId: 1,
+      productImage: 'img1.jpg',
+      productName: 'Jacket',
+      qty: 2,
 
+    },
+  ];
+
+  pastOrder: PastOrder = {
+    address: 'Address goes here',
+    city: 'New Jersey',
+    orderDate: '03/01/23',
+    pin: '12345',
+    state: 'NY',
+    total: 100,
+    userName: 'Pooja',
+  };
+
+  selectOrder(event: any){
+    
+  }
 }
